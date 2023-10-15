6bf5b1dbc8e9616f64ab341aae6ac1c0e4cbc631
AnatoliiGady <grek-prost@yandex.ru>
2023-10-15 22:16:10 +0900

    Ознакомительные задачи

--- src/main/java/ru/job4j/io/ReadFile.java
+++ src/main/java/ru/job4j/io/ReadFile.java
@@ -1,21 +1,15 @@
 package ru.job4j.io;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class ReadFile {
     public static void main(String[] args) {
-        try (FileInputStream in = new FileInputStream("data/input.txt")) {
-            StringBuilder text = new StringBuilder();
-            int read;
-            while ((read = in.read()) != -1) {
-                text.append((char) read);
-            }
-            String[] lines = text.toString().split(System.lineSeparator());
-            for (String line : lines) {
-                System.out.println(line);
-            }
-        } catch (IOException e) {
+        try (BufferedReader in = new BufferedReader(new FileReader("data/input.txt"))) {
+            in.lines().forEach(System.out::println);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
