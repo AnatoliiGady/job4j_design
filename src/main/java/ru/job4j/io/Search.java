7b892085a5cf8819a64143856853342ee5a152c7
AnatoliiGady <grek-prost@yandex.ru>
2023-12-16 14:08:57 +0900

    12. Валидация параметров запуска

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,5 +1,6 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -9,8 +10,9 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith(".js")).forEach(System.out::println);
+        validParameter(args);
+        Path start = Paths.get(args[0]);
+        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
 
     }
 
@@ -18,5 +20,19 @@ public class Search {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    public static void validParameter(String[] args) {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("incorrect number of arguments");
+        }
+        File file = new File(args[0]);
+        if (!file.exists()) {
+            throw new IllegalArgumentException("root folder should not be null");
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException("search extension is empty, enter file extension");
+        }
+
     }
 }
