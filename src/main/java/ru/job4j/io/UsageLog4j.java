85938021518b3eaaf7101e785fab2c5424159918
AnatoliiGady <grek-prost@yandex.ru>
2024-01-07 20:10:27 +0900

    3. Slf4j - вывод переменных.

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,17 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String name = "Anatolii Gadi";
+        byte age = 33;
+        double weight = 90.3;
+        float height = 1.76f;
+        int salary = 150000;
+        char beginsName = 'A';
+        boolean isProgrammer = true;
+        long exerciseNumber = 15345674867L;
+        short dayTraining = 531;
+        LOG.debug("User info name : {}, age : {}, weight : {}, height : {}, salary : {}, beginsName : {}, isProgrammer : {}",
+                name, age, weight, height, salary, beginsName, isProgrammer);
+        LOG.debug("training information : dayTraining : {}, exerciseNumber : {}", dayTraining, exerciseNumber);
     }
 }
