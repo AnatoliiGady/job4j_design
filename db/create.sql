dae9851544ea81c64cfa2d6ff2d09277a55a98a3
AnatoliiGady <grek-prost@yandex.ru>
2024-03-17 16:04:13 +0900

    2. Схема прав пользователи и роли.

--- db/create.sql
+++ db/create.sql
@@ -6,20 +6,12 @@ create table roles (
 create table users (
 	id serial primary key,
 	name varchar(255),
-	user_id int references users(id)
+	role_id int references roles(id)
 );
 
 create table rules (
 	id serial primary key,
 	name varchar(255)
-);
-
-create table items (
-	id serial primary key,
-	name varchar(255),
-	user_id int references users(id),
-	category_id int references categories(id),
-    state_id int references states(id)
 );
 
 create table comments (
@@ -44,10 +36,16 @@ create table categories (
 	name varchar(255)
 );
 
+create table items (
+	id serial primary key,
+	name varchar(255),
+	user_id int references users(id),
+	category_id int references categories(id),
+    state_id int references states(id)
+);
+
 create table roles_rules (
 	id serial primary key,
 	role_id int references roles(id),
 	rule_id int references rules(id)
 );
-
-
